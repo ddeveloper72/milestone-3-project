24cd5ad2b945369abfab552ac07cc0220f1034b3
Duncan Falconer <duncan.niamh@gmail.com>
2018-07-14 14:15:57 +0100

    refined added a riddles.json file which includes image links. completed first two successful tests with corresponding functions.

--- byotest.py
+++ byotest.py
@@ -1,14 +1,38 @@
-"""
-def array_of_numbers(numbers):
-    return 1
-"""
+import unittest
+import run
 
-"""
-Test the user selection for the game
-"""
-def test_are_equal(actual, expected):
-    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+class TestStringMethods(unittest.TestCase):
+    #samle tests  https://docs.python.org/3/library/unittest.html
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
     
-""" 
-test_are_equal(array_of_numbers([1,2,3,4,5,6]), 2)
-"""
+    #Tests for geenarsing game code:
+    def test_loadUsers(self):
+        """
+        #test to check that the users can be loaded from the
+        #json file
+        """
+        users = run.loadUsers()
+        self.assertEqual(len(users),3)
+
+    def test_loadRiddles(self):
+        """
+        #test to check that the users can be loaded from the
+        #json file
+        """
+        riddles = run.loadRiddles()
+        self.assertEqual(len(riddles),14)
+
+if __name__ == '__main__':
+    unittest.main()
--- run.py
+++ run.py
@@ -1,54 +1,20 @@
-from byotest import *
+import json
 
-"""
-Users will select the game mode, 1 or 2 to either play the game or quit
-"""
-def game_mode_selection():
-    print("To play 'Riddle-Me-This', press 1")
-    print("Press 2, to Quit")
-    
-    option = input("What's your selection: ")
-    return option
-    
-    
-def ask_riddles():
-    riddles = []
-    answers = []
-    
-    with open("riddles.txt", "r") as file:
-        lines = file.read().splitlines()
-        
-    for i, text in enumerate(lines):
-        if  i%2 == 0:
-            riddles.append(text)
-        else:
-            answers.append(text)
-            
-    number_of_riddles = len(riddles)
-    riddles_and_answers = zip(riddles, answers)
-    
-    score = 0
-    
-    for riddle, answer in riddles_and_answers:
-        guess = input(riddle + "> ")
-        if guess == answer:
-            score += 1
-            print("Brilliant answer!")
-            print(score)
-        else:
-            print("No, thats wrong!")
-        
-    print("you got {0} correct out of {1}".format(score, number_of_riddles))
+def loadUsers():
+    """ 
+    Lets get our player names from our databse file: 
+    """
+    with open("data/users.json", "r") as file:
+        data = json.load(file)
+        return data
 
-def game_loop():
-    while True:
-        option = game_mode_selection()
-        if option == "1":
-            ask_riddles()
-        elif option == "2":
-            break
-        else:
-            print("Invalid option")   
-        print("")
-        
-game_loop()
+def loadRiddles():
+    """ 
+    Read the riddles from the riddles txt: 
+    """
+    with open("data/riddles.json", "r") as file:
+        data = json.load(file)
+        return data
+
+
+
