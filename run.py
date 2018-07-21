ff980451885097089c82644b11f59fa42d75bddf
Duncan Falconer <duncan.niamh@gmail.com>
2018-07-21 22:53:09 +0100

    Began work on SQLAlchemy & styling html templates.

--- run.py
+++ run.py
@@ -1,25 +1,35 @@
 import os
 import json
 from flask import Flask, redirect, render_template, request, flash, session, abort
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Length
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-Bootstrap(app)
 app.secret_key = 'some_secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/dunca/OneDrive/The Code Institute/My Visual Studio Projects/milestone-3-project/database.db'
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    password = db.Column(db.String(80))
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
 
-class SignupForm(FlaskForm):
+class SignUpForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-
+    
 
 def write_to_file(filename, data):
     #Handel the process of writing data to a text file
@@ -93,24 +103,34 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login(): 
-    form = LoginForm()      
+    form = LoginForm() 
+
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
     return render_template('login.html', form = form)
 
 
-@app.route('/signup')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = SignupForm()
+    form = SignUpForm()
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
     return render_template('signup.html', form=form)
     
 
 @app.route('/game')
+@login_required
 def game():   
     return render_template("game.html")   
 
 
 @app.route('/leaderboard')
+@login_required
 def leaderboard():
     return render_template("leaderboard")
 
