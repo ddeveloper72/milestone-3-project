be50ba688bf932cd64f7d65a5b437a4421521035
Duncan Falconer <duncan.niamh@gmail.com>
2018-08-23 14:20:10 +0000

    Removed brackets from Leaderboard return function for first, second, third

--- run.py
+++ run.py
@@ -228,7 +228,7 @@ def scores_list():
     first = str(li[0])[1:-1].replace("'" , " ").replace("," , " ")
     second = str(li[1])[1:-1].replace("'" , " ").replace("," , " ")
     third = str(li[2])[1:-1].replace("'" , " ").replace("," , " ")
-    return(first, second, third)     
+    return first, second, third    
     
 
 # The Flask decorators below, process and render data to our front end templates.
